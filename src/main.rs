cf2e5e5e0cf0b3f6f5c5415f13169c7a9daa03c2
Minyewoo <const.trushov@gmail.com>
2023-10-25 19:58:11 +0300

    Added resets with timers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::time::Duration;
+
 use opencv::{highgui, videoio,  prelude::*, imgproc::{self, LINE_8}, core::{Scalar, Vector, Point, BORDER_DEFAULT, lut, CV_8U, CV_16S, convert_scale_abs, Size2i, Point_}, imgcodecs::IMREAD_GRAYSCALE,};
+use simpler_timer::Timer;
 fn contours_matching() -> Result<(), String> {
     let name = "test";
     highgui::named_window(name, highgui::WINDOW_GUI_NORMAL | highgui::WINDOW_KEEPRATIO | highgui::WINDOW_AUTOSIZE)
@@ -30,19 +33,29 @@ fn contours_matching() -> Result<(), String> {
     let mut look_up_table = Mat::default();
     let mut bytes_image = Mat::default();
     let mut last_similarity = 0.0;
-    let mut last_centroid = Point_::<f32>::default();
-    make_look_up_table(&mut look_up_table, 0.5)?;
+    let mut last_centroid = Point_::<f32>::new(0.0, 0.0);
+    let similarity_reset_timer = Timer::with_duration(Duration::from_millis(800));
+    let centroid_reset_timer = Timer::with_duration(Duration::from_millis(800));
+    make_look_up_table(&mut look_up_table, 0.8)?;
     loop {
+        if similarity_reset_timer.expired() {
+            last_similarity = 0.0;
+            similarity_reset_timer.reset();
+        }
+        if centroid_reset_timer.expired() {
+            last_centroid = Point_::<f32>::new(0.0, 0.0);
+            centroid_reset_timer.reset();
+        }
         capture.read(&mut frame)
             .map_err(|err| format!("[main] failed to read a frame from VideoCaprute: {err}"))?;
         imgproc::cvt_color(&frame, &mut grayscale_frame, imgproc::COLOR_BGR2GRAY, 0)
             .map_err(|err: opencv::Error| format!("[main] failed to convert frame to grayscale: {err}"))?;
         // convert_scale_abs(&grayscale_frame, &mut contrast_grayscale, 0.5, 10.0).map_err(|err| format!("[main] convert_scale_abs: {err}"))?;
-        // lut(&grayscale_frame, &look_up_table, &mut contrast_grayscale)
-        //     .map_err(|err: opencv::Error| format!("[main] lut: {err}"))?;
+        lut(&grayscale_frame, &look_up_table, &mut contrast_grayscale)
+            .map_err(|err: opencv::Error| format!("[main] lut: {err}"))?;
         // imgproc::gaussian_blur(&grayscale_frame, &mut frame_smoothed, Size2i::new(3, 3), 0.0, 0.0, BORDER_DEFAULT)
         //     .map_err(|err| format!("[main] gaussian_blur: {err}"))?;
-        imgproc::bilateral_filter(&grayscale_frame, &mut bilateral_filtered, 5, 75.0, 75.0, BORDER_DEFAULT)
+        imgproc::bilateral_filter(&contrast_grayscale, &mut bilateral_filtered, 5, 75.0, 75.0, BORDER_DEFAULT)
             .map_err(|err| format!("[main] bilateral_filter: {err}"))?;
         // imgproc::laplacian(&frame_smoothed, &mut edges, CV_16S, 3, 1.0, 0.0, BORDER_DEFAULT)
         //     .map_err(|err| format!("[main] laplacian: {err}"))?;
@@ -57,7 +70,8 @@ fn contours_matching() -> Result<(), String> {
             .iter()
             .filter(|contour| {
                 let area = imgproc::contour_area(contour, false).unwrap();
-                area > 450.0
+                // println!("{}", area);
+                area > 200.0
             })
             .map(|contour| {
                 let moments = imgproc::moments(&contour, false)
@@ -78,7 +92,7 @@ fn contours_matching() -> Result<(), String> {
                     },
                     _ => {
                         let similarity_distance = (last_similarity - similarity).abs();
-                        if similarity_distance <= 0.02 {
+                        if similarity < last_similarity || similarity_distance <= 0.1 {
                             last_similarity = similarity;
                             true
                         } else {
@@ -90,27 +104,34 @@ fn contours_matching() -> Result<(), String> {
             .filter(|contour| {
                 let centroid= imgproc::min_area_rect(&contour.1).unwrap().center;
                 let centroids_distance = ((last_centroid.x - centroid.x).powf(2.0)  + (last_centroid.y - centroid.y).powf(2.0)).sqrt();
-                println!("{}", centroids_distance);
-                if centroids_distance <= 150.0 || last_centroid == (Point_ { x: 0.0, y: 0.0 })  {
+                // println!("{}", centroids_distance);
+                if centroids_distance <= 70.0 || last_centroid == (Point_ { x: 0.0, y: 0.0 })  {
                     last_centroid = centroid;
+                    centroid_reset_timer.reset();
                     true
                 } else {
                     false
                 }
             })
-            .fold((0.15, Vector::<Point>::default()), |min_contour, contour| {
+            .fold((0.2, Vector::<Point>::default()), |min_contour, contour| {
                 if contour.0 < min_contour.0 {
-                    // println!("{}", contour.0);
+                    println!("{}", contour.0);
                     contour
                 } else {
                     min_contour
                 }
             }).1;
         if matched_contour.len() > 0 {
+            let area = imgproc::contour_area(&matched_contour, false).unwrap();
+            // println!("{}", area);
+            similarity_reset_timer.reset();
+            centroid_reset_timer.reset();
             let mut matched_contours = Vector::<Vector<Point>>::default();
             matched_contours.push(matched_contour);
             imgproc::draw_contours(&mut frame, &matched_contours, -1, Scalar::new(0.0, 255.0, 0.0, 255.0), 2, LINE_8, &Mat::default(), 0, Point::new(0, 0))
                 .map_err(|err| format!("[main] draw_contours: {err}"))?;
+            imgproc::circle(&mut frame, Point::new(last_centroid.x as i32, last_centroid.y as i32), 2, Scalar::new(0.0, 0.0, 255.0, 255.0), 2, LINE_8, 0)
+                .map_err(|err| format!("[main] circle: {err}"))?;
         }
         highgui::imshow(name, &frame)
             .map_err(|err| format!("[main] failed to display an image in the window: {err}"))?;
@@ -143,7 +164,5 @@ fn make_look_up_table(look_up_table: &mut Mat, gamma: f64) -> Result<(), String>
 }
 
 fn main() -> Result<(), String> {
-    // haar_cascade()
-    // brute_force_matching_orb()
     contours_matching()
 }
